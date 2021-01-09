7143d3110694a891dcce55dd03dece086187591f
Emad A. Ismael <EmadAIsmael@yahoo.com>
2021-01-10 01:30:50 +0200

    Jan 10, 2021 01:30

--- Seam Carving/task/src/seamcarving/Main.kt
+++ Seam Carving/task/src/seamcarving/Main.kt
@@ -1,44 +1,50 @@
 package seamcarving
 
 import java.awt.Color
-import java.awt.Graphics2D
-import java.awt.image.BufferedImage
+//import java.awt.Graphics2D
+//import java.awt.image.BufferedImage
 import java.io.File
-import java.util.*
+//import java.util.*
 import javax.imageio.ImageIO
 
 
-fun main() {
-    val scanner = Scanner(System.`in`)
+fun main(args: Array<String>) {
 
-    print("Enter rectangle width:")
-    val width = scanner.nextInt()
+    // 1. Read arguments
+    if (args.size != 4) {
+        println("Invalid number of arguments.")
+        return
+    }
+    val inFile = args[args.indexOf("-in") + 1]
+    val outFile = args[args.indexOf("-out") + 1]
 
-    print("Enter rectangle height:")
-    val height = scanner.nextInt()
+    // 2. read input file; get image width & height
+    val theFile = File(inFile)
+    val inImg = ImageIO.read(theFile)
+    val width = inImg.width
+    val height = inImg.height
 
-    print("Enter output image name:")
-    val fileName = scanner.next()
+    // 3. manipulate pixels
+    for (x in 0 until width)
+        for (y in 0 until height) {
+            val color = inImg.getRGB(x, y)
+            // unpack color int
+            val a = (color shr 24) and 0xff
+            val r = (color shr 16) and 0xff
+            val g = (color shr 8) and 0xff
+            val b = (color) and 0xff
+            // pack new color int
+            val newColor =
+                (((255 - a) and 0xff) shl 24) or
+                        (((255 - r) and 0xff) shl 16) or
+                        (((255 - g) and 0xff) shl 8) or
+                        (((255 - b) and 0xff)
+                                )
+            inImg.setRGB(x, y, newColor)
+        }
 
-    // 1. create image
-    val img = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
-
-    // 2. draw shapes
-    val g: Graphics2D = img.createGraphics()
-    g.color = Color.black
-    g.fillRect(0, 0, width - 1, height - 1)
-
-    // drawLine(int x1, int y1, int x2, int y2)
-    g.color = Color.red
-    val (x1, y1, x2, y2) = listOf(0, 0, width - 1, height - 1)
-    g.drawLine(x1, y1, x2, y2)
-    g.drawLine(x2, y1, x1, y2)
-
-    // 3. save image
-    val f = File(fileName)
-    ImageIO.write(img, "PNG", f)
-
-    // 4. clean up
-    g.dispose()
+    // 4. save to ouput image file
+    val f = File(outFile)
+    ImageIO.write(inImg, "PNG", f)
 
 }
